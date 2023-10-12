cafe1cd2f0e1887b911699ec0e9471f599d48e33
Max <140860201+rango333@users.noreply.github.com>
2023-10-11 18:28:55 -0600

    Add files via upload

---
+++ block_letters.py
@@ -0,0 +1,10 @@
+print("Max Santomauro")
+print("Fun Fact: I am a cybersecurity professional")
+# print initials
+print("M   M IIIII  SSS")
+print("MM MM   I   S   S")
+print("MM MM   I   S")
+print("M M M   I    SSS")
+print("M   M   I       S")
+print("M   M   I   S   S")
+print("M   M IIIII  SSS")block_
